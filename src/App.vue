05159cebb31b1f587af339d0995a49b2db30d72b
Graham Swan <thinkswan@gmail.com>
2020-06-29 11:31:47 +0200

    vue-router scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,32 @@
 <template>
   <div id="app">
-    <Header />
-    <AddTodo @add-item="addItem" />
-    <TodoList :todos="todos" @delete-item="deleteItem" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import axios from "axios";
-import Header from "./components/layouts/Header";
-import AddTodo from "./components/AddTodo";
-import TodoList from "./components/TodoList";
-
-const API_BASE = "https://jsonplaceholder.typicode.com/todos";
-
-export default {
-  name: "App",
-  components: { Header, AddTodo, TodoList },
-  data() {
-    return {
-      todos: []
-    };
-  },
-  methods: {
-    async addItem(newTodo) {
-      try {
-        const { title, completed } = newTodo;
-
-        const response = await axios.post(API_BASE, {
-          title,
-          completed
-        });
-
-        this.todos = [...this.todos, response.data];
-      } catch (error) {
-        console.error(error);
-      }
-    },
-
-    async deleteItem(id) {
-      try {
-        await axios.delete(`${API_BASE}/${id}`);
-
-        this.todos = this.todos.filter(item => item.id !== id);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  },
-  async created() {
-    try {
-      const response = await axios.get(`${API_BASE}?_limit=5`);
-
-      this.todos = response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-};
-</script>
-
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
+#nav {
+  padding: 30px;
 }
 
-.button {
-  display: inline-block;
-  border: none;
-  background-color: #555;
-  color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-.button:hover {
-  background-color: #666;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
